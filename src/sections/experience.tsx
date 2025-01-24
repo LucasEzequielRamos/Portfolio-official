19388de96bb134917bcfdd6ae196dfcaae4f9924
Lucas Becerra <lucaseramos13@gmail.com>
2025-01-24 19:53:17 -0300

    change experience and add new cv

--- src/sections/experience.tsx
+++ src/sections/experience.tsx
@@ -12,25 +12,25 @@ const Experience = () => {
         <div className=" grid justify-between lg:w-10/12  gap-2 text-sm md:text-base 2xl:text-lg text-center grid-cols-2 mx-auto [&>div]:border-l-4 [&>div]:px-2 [&>div>h3]:text-xl [&>div>h3]:md:text-2xl [&>div>h3]:2xl:text-3xl [&>div>h3]:text-primary [&>div>h3]:font-bold ">
           <div>
             <h3>
-              +<NumberTicker value={2}  />
+              +<NumberTicker value={2} />
             </h3>
             <p>Años de experiencia</p>
           </div>
           <div>
             <h3>
-              <NumberTicker value={2}  />
+              <NumberTicker value={2} />
             </h3>
             <p>Cliente satisfecho</p>
           </div>
           <div>
             <h3>
-              <NumberTicker value={2}  />
+              <NumberTicker value={2} />
             </h3>
             <p>Proyecto finalizado</p>
           </div>
           <div>
             <h3>
-              <NumberTicker value={1} delay={1} />
+              <NumberTicker value={0} delay={1} />
             </h3>
             <p>Proyectos en curso</p>
           </div>
@@ -42,7 +42,7 @@ const Experience = () => {
               Developer
             </h3>
             <h4 className="text-xs md:text-sm 2xl:text-base mt-2">
-              Octubre 2022 - Actualidad
+              Octubre 2022 - Octubre 2024
             </h4>
             <p className="mt-4 text-left tracking-wide md:tracking-normal">
               Hasta el momento, he trabajado en un proyecto profesional
